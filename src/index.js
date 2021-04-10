404ed84baa308b2d13a796086bd771b5964dcd40
ewilsandman <56822177+ewilsandman@users.noreply.github.com>
2021-04-10 15:16:55 +0200

    Hello world styled

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
- var element = React.createElement('h1', {className: 'greeting'}, 'Hello, world!');
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+ const element = React.createElement(Title, null, 'Hello, world!');
+ 
  ReactDOM.render(element, document.getElementById('root'));
+ 
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+//reportWebVitals();
