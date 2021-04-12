4fef7732f079934f06ac73d4ab850ccc99803bd2
ewilsandman <56822177+ewilsandman@users.noreply.github.com>
2021-04-12 14:24:06 +0200

    now more functions

--- src/index.js
+++ src/index.js
@@ -2,17 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
+
+const Background = styled.body`
+  background-color: green;
+`
 
 const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
   color: palevioletred;
 `;
- const element = React.createElement(Title, null, 'Hello, world!');
+
+function Hmm(){
+  return <Title>OwO</Title>;      
+}
+
+function Ono(){
+  return <Background>Whats this?</Background>;
+}
  
- ReactDOM.render(element, document.getElementById('root'));
- 
+//const test2 = React.createElement(Background, null, 'This be a test');
+
+ReactDOM.render(
+  <div>
+    <Hmm />
+    <Ono />
+  </div>,
+document.getElementById('root'),
+);
+
+// ReactDOM.render(test2, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
